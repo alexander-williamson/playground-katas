abb5d5ff6a43e57821535d138921b7ce80096df0
Alexander Williamson <alex@alexw.co.uk>
2025-03-08 22:16:50 +0000

    121 and 189

---
+++ leetcode/121-best-time-to-buy-and-sell-stock/README.md
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1567431651/?envType=study-plan-v2&envId=top-interview-150
+
+Found this one really tough
+
+Way to solution is to use Math.MAX or Infinity then work out the profit in between the prices.
---
+++ leetcode/121-best-time-to-buy-and-sell-stock/code.ts
@@ -0,0 +1,17 @@
+function maxProfit(prices: number[]): number {
+    let maxProfit = 0;
+    let minPrice = Infinity;
+    let highest = prices[0];
+
+    for(let i = 0; i < prices.length; i++) {
+        let currProfit = prices[i] - minPrice; // massive
+        if(prices[i] < minPrice) {
+            minPrice = prices[i]
+        }
+        if(currProfit > maxProfit) {
+            maxProfit = currProfit;
+        }
+    }
+
+    return maxProfit;
+};
