ac14f9b88fd492b269e4a54041191468c6490b0d
Alex Williamson <alexander.williamson@gmail.com>
2026-02-08 10:09:16 +0000

    Use bitwise peeling of numbers

--- katas/9-palindrome-number/palindrome.test.ts
+++ katas/9-palindrome-number/palindrome.test.ts
@@ -1,27 +1,31 @@
 import { describe, expect, it } from "bun:test";
 
 function isPalindrome(x: number): boolean {
-  const chars = String(x).split("");
-  let firstPos = 0;
-  let lastPos = chars.length - 1;
-  while (firstPos < lastPos) {
-    if (chars[firstPos] !== chars[lastPos]) {
-      return false;
-    }
-    firstPos++;
-    lastPos--;
+  if (x < 0 || (x % 10 === 0 && x !== 0)) {
+    return false;
   }
-  return true;
+
+  // use mod % 10 to peel off the last digit !!
+  // use bitwise "| 0" instead of Math.floor
+
+  let reversedHalf = 0;
+
+  while (x > reversedHalf) {
+    reversedHalf = reversedHalf * 10 + (x % 10);
+    x = (x / 10) | 0; // bitwise truncation faster than Math.floor
+  }
+
+  // for even we'll get a same length
+  // for odd, chop a digit off
+  return x === reversedHalf || x === ((reversedHalf / 10) | 0);
 }
 
 describe("isPalindrome", () => {
-  it("returns true for palindroms", () => {
-    expect(isPalindrome(1)).toBeTrue();
-    expect(isPalindrome(121)).toBeTrue();
+  it.each([1, 121])("returns true for palindroms - %d", (testCase) => {
+    expect(isPalindrome(testCase)).toBeTrue();
   });
 
-  it("returns false for non palindromes", () => {
-    expect(isPalindrome(-121)).toBeFalse();
-    expect(isPalindrome(-121)).toBeFalse();
+  it.each([10, -121, 1222])("returns false for non palindromes - %d", (testCase) => {
+    expect(isPalindrome(testCase)).toBeFalse();
   });
 });
